c2f43e2988c5a2259be7b981d9dea38f0e592475
Bence Pentek <pntkbence@gmail.com>
2022-06-06 14:09:37 +0200

    Creating login,showing users data

---
+++ src/js/data.js
@@ -0,0 +1,13 @@
+const users = [
+    {
+        username: 'bence pentek',
+        pin: '1111',
+        movements: [500, 230, -320, 1000, 550],
+    },
+    {
+        username: 'test',
+        pin: '2222',
+        movements: [500, 450, 2000, -350, -200, 25000],
+    },
+
+];
--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,133 @@
-const signInBtn = document.querySelector('#signin');
-const loginForm = document.querySelector('#loginform');
+
 const homePage = document.querySelector('#homepage');
 const landing = document.querySelector('#landing');
-const closeBtn = document.querySelector('#closeBtn');
 const userProfile = document.querySelector('#user-profile');
 
-signInBtn.addEventListener('click', function () {
-    loginForm.classList.toggle('hidden');
-    landing.classList.toggle('hidden');
-})
+// Login Form Elements
+const closeBtn = document.querySelector('#closeBtn');
 
+const loginForm = document.querySelector('#loginform');
+const signInBtn = document.querySelector('#signin');
+
+const usernameInput = document.querySelector('#username');
+const pinInput = document.querySelector('#pin');
+const formSignInBtn = document.querySelector('#formSignInBtn');
+
+
+// Transactions
+const transactionsContainer = document.querySelector('#transactionsContainer');
+
+const logoutBtn = document.querySelector('#logoutBtn');
+
+const welcomeName = document.querySelector('#welcomeName');
+const balanceSpan = document.querySelector('#balance');
+const incomeLabel = document.querySelector('#incomeLabel');
+const outcomeLabel = document.querySelector('#outcomeLabel');
+// Hide User
+// userProfile.classList.toggle('hidden');
+
+
+// Clear values
+incomeLabel.innerHTML = '';
+outcomeLabel.innerHTML = '';
+
+
+// Close Login Form
 closeBtn.addEventListener('click', function () {
     loginForm.classList.toggle('hidden');
     landing.classList.toggle('hidden');
 })
 
 
-userProfile.classList.toggle('hidden');
+//  Show Login Form
+signInBtn.addEventListener('click', function () {
+    loginForm.classList.toggle('hidden');
+    landing.classList.toggle('hidden');
+
+})
+
+
+
+// Logout
+logoutBtn.addEventListener('click', function () {
+    userProfile.classList.toggle('hidden');
+    loginForm.classList.toggle('hidden');
+    landing.classList.toggle('hidden');
+    homePage.classList.toggle('hidden');
+    document.body.classList.toggle('bg');
+
+
+})
+
+
+// Load transactions
+const transactionMovements = function (movements) {
+    transactionsContainer.innerHTML = '';
+
+    movements.forEach((mov, i) => {
+        const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+        const htmlStr = `<div class="transaction-html pt-4 pb-3 w-full">
+                        <div class="transaction-inner-div">
+                            <p class="${type}"><span>${i + 1}</span> ${type}</p>
+                        </div>
+                        <div class="money-amount">
+                            <p class="text-xl"> <span>$</span>${Math.abs(mov)}</p>
+                        </div>
+                    </div>`;
+        transactionsContainer.insertAdjacentHTML('afterbegin', htmlStr);
+    })
+}
+
+
+
+const displayBalance = function (movements) {
+    const balance = movements.reduce((acc, curr) => acc + curr, 0);
+    balanceSpan.innerHTML = balance
+
+}
+
+const calcBalanceSummary = function (movements) {
+    const income = movements.filter(mov => mov > 0).reduce((a, b) => a + b, 0);
+    incomeLabel.innerHTML = `$${income}`;
+
+    const outcome = movements.filter(mov => mov < 0).reduce((a, b) => a + b, 0);
+    outcomeLabel.innerHTML = `$${Math.abs(outcome)}`;
+}
+
+
+
+// Login
+let currAccount;
+
+formSignInBtn.addEventListener('click', function () {
+
+    currAccount = users.find(acc => acc.username === usernameInput.value);
+
+    if (currAccount?.pin === pinInput.value) {
+        usernameInput.value = pinInput.value = '';
+        homePage.classList.toggle('hidden');
+        userProfile.classList.toggle('hidden');
+        document.body.classList.toggle('bg');
+
+        // Display users name
+        // let displayedUsername = currAccount.username.split(' ');
+        // displayedUsername = displayedUsername.forEach(word => word[0].toUpperCase());
+
+
+        welcomeName.innerHTML = currAccount.username;
+
+        // Display balance
+        displayBalance(currAccount.movements)
+        // Display transactions
+        transactionMovements(currAccount.movements);
+        // Display summary
+        calcBalanceSummary(currAccount.movements);
+    }
+    else {
+        console.log('error');
+    }
+
+
+
+});
